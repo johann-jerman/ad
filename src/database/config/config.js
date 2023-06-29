74d1c6d74a9f205ca5f5371ff97486619f5b8ecd
unknown <johannjerman014@gmail.com>
2023-06-29 17:21:10 -0300

    a

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -8,18 +8,18 @@ module.exports =
     "host": process.env.DB_HOST,
     "dialect": process.env.DB_MOTOR || "postgres",
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
+  // "test": {
+  //   "username": "root",
+  //   "password": null,
+  //   "database": "database_test",
+  //   "host": "127.0.0.1",
+  //   "dialect": "mysql"
+  // },
+  // "production": {
+  //   "username": "root",
+  //   "password": null,
+  //   "database": "database_production",
+  //   "host": "127.0.0.1",
+  //   "dialect": "mysql"
+  // }
 }
