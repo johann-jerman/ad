885b3204ab824a42536f831300f132dfecd96de4
unknown <johannjerman014@gmail.com>
2023-06-29 17:30:36 -0300

    a

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 module.exports = 
 {
   "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
+    "username": process.env.DB_USERNAME || "liza_db_user",
+    "password": process.env.DB_PASSWORD || "o8jwygzr2N6jIsWhV6lTDXnGxLz0yaFv",
+    "database": process.env.DB_DATABASE || "liza_db",
+    "host": process.env.DB_HOST || "postgres://liza_db_user:o8jwygzr2N6jIsWhV6lTDXnGxLz0yaFv@dpg-cietkrh5rnujc4p7icl0-a/liza_db",
     "dialect": process.env.DB_MOTOR || "postgres",
   },
   // "test": {
