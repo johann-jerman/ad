b5c95926af9678205c2ec8962f128121cd6a28d0
unknown <johannjerman014@gmail.com>
2023-06-29 19:16:18 -0300

    a

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -20,7 +20,7 @@ let sequelize;
   sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: process.env.DB_MOTOR
-  }).catch(e => console.log(e));
+  });
 
   console.log(
     process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, process.env.DB_HOST,process.env.DB_MOTOR
