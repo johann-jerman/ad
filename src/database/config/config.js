86b7a04471e88650c0831c6422cd1567ca6969f2
unknown <johannjerman014@gmail.com>
2023-07-01 15:42:43 -0300

    a

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,19 +2,15 @@ require('dotenv').config()
 module.exports = 
 {
   development: {
-    username: 'liza_db_user',
-    password: 'o8jwygzr2N6jIsWhV6lTDXnGxLz0yaFv',
-    database: 'liza_db',
-    host: 'dpg-cietkrh5rnujc4p7icl0-a',
+    username: 'liza_db_w2gf_user',
+    password: 'T1LHQNYRZZlsahR0iL8zrMFu8qwA2QL4',
+    database: 'liza_db_w2gf',
+    host: 'dpg-cig6aq5gkuvojjfsr790-a',
     dialect: 'postgres',
     migrationStorage: 'sequelize',
     migrationStorageTableName: 'sequelize_meta',
     seederStorage: 'sequelize',
     seederStorageTableName: 'sequelize_data',
-    define: {
-      timestamps: false,
-      underscored: true,
-    },
     migrationStoragePath: 'src/database/migrations',
     seederStoragePath: 'src/database/seeders',
   },
