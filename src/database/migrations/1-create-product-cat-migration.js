72ab10eb06e875129d4afafa6b1ab0554781c68a
unknown <johannjerman014@gmail.com>
2023-07-01 14:31:34 -0300

    a

--- src/database/migrations/1-create-product-cat-migration.js
+++ src/database/migrations/1-create-product-cat-migration.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Categorysproduct', {
+    await queryInterface.createTable('CategorysProduct', {
       id: {
         allowNull: false,
         autoIncrement: true,
--- src/database/migrations/1-create-user-cat-migration.js
+++ src/database/migrations/1-create-user-cat-migration.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Usercategory', {
+    await queryInterface.createTable('UserCategory', {
       id: {
         allowNull: false,
         autoIncrement: true,
