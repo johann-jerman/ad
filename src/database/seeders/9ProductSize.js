69096fada9959f3288f447c1ce7662db0b130cd2
unknown <johannjerman014@gmail.com>
2023-07-01 16:54:50 -0300

    a

--- src/database/seeders/9ProductSize.js
+++ src/database/seeders/9ProductSize.js
@@ -26,42 +26,42 @@ module.exports = {
       {
         size_id: 6,
         product_id: 1,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 2,
         product_id: 2,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 5,
         product_id: 2,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 6,
         product_id: 2,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 2,
         product_id: 3,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 5,
         product_id: 3,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
@@ -69,196 +69,196 @@ module.exports = {
         size_id: 6,
         
         product_id: 3,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 2,
         product_id: 4,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 5,
         product_id: 4,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 6,
         product_id: 4,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 5,
         product_id: 5,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 6,
         product_id: 5,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 5,
         product_id: 6,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 6,
         product_id: 6,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 5,
         product_id: 7,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 6,
         product_id: 7,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 5,
         product_id: 8,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 6,
         product_id: 8,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 5,
         product_id: 9,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 6,
         product_id: 9,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 5,
         product_id: 10,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 6,
         product_id: 10,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 5,
         product_id: 11,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 6,
         product_id: 11,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 5,
         product_id: 12,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 6,
         product_id: 12,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 5,
         product_id: 13,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 6,
         product_id: 13,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 5,
         product_id: 14,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 6,
         product_id: 14,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 5,
         product_id: 15,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 6,
         product_id: 15,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 5,
         product_id: 16,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
       {
         size_id: 6,
         product_id: 16,
-        createdAt: new Date(),
+        created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null
       },
